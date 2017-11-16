4c1b33075d04e1853bb0dfe184ec82c529c88082
Appliberated <develop@appliberated.com>
2017-11-16 18:31:55 +0200

    Add emoji copy and snackbar functionality

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
-const list = document.querySelectorAll(".emoji-list li");
-for (var item of list) item.tabIndex = 0;
+
+(function Emojimalist() {
+
+
+    let copiedEmoji = document.getElementById("copied-emoji");
+    let snackbar = document.getElementById("snackbar");
+
+    function showSnackbar(message) {
+        snackbar.innerText = message;
+        snackbar.classList.remove("show");
+        snackbar.offsetHeight;
+        snackbar.classList.add("show");
+    }
+
+    function handleClick(event) {
+        if (event.target.nodeName === "I") {
+            copiedEmoji.value = event.target.innerText;
+            copiedEmoji.select();
+            document.execCommand("copy");
+            showSnackbar("Emoji copied");
+        }
+    }
+
+    const list = document.querySelectorAll(".emoji-list li");
+    for (var item of list) item.tabIndex = 0;
+
+
+    let emojiList = document.querySelector(".emoji-list");
+    emojiList.addEventListener("click", handleClick);
+
+}());
